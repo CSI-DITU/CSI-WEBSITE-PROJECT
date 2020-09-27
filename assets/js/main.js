90a53e9cbd1ef081dd8159deb39a976780be6c2b
Uttam Rao <uttamrao2000@gmail.com>
2020-09-27 21:54:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -205,7 +205,6 @@
   });
 
 })(jQuery);
-<<<<<<< HEAD
 
 // Gallery Animation
 $(function() {
@@ -222,17 +221,3 @@ $(function() {
   });
 
   //Gallery Animations end
-=======
-$(function() {
-var selectedClass = "";
-$(".filter").click(function(){
-selectedClass = $(this).attr("data-rel");
-$("#gallery").fadeTo(100, 0.1);
-$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-setTimeout(function() {
-$("."+selectedClass).fadeIn().addClass('animation');
-$("#gallery").fadeTo(300, 1);
-}, 300);
-});
-});
->>>>>>> cbcb4df68b5c28e248f3bbc593aced3eb93c7de7
